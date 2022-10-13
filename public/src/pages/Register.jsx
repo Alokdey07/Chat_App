dd9bdb5cfee392a5da679d2530192f79927473fa
Alokdey07 <alokdeykv@gmail.com>
2022-10-13 11:18:12 +0530

    alok

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,38 +1,80 @@
 import React,{useState,useEffect} from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from "../assets/logo.png";
 import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from '../utils/APIRouts';
 
 
 function Register() {
+
+    const navigate = useNavigate();
 
     const [values,setValues] = useState({
         username: "",
         email: "",
         password: "",
         confirmPassword: "",
-    })
+    });
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
 
-    const handleSubmit = (event)=>{
+    }
+
+    useEffect(() => {
+        if(localStorage.getItem('chat-app-user')){
+          navigate('/')
+        }
+      },[])
+
+    const handleSubmit = async (event)=>{
         event.preventDefault();
-        handleValidation();
+       if(handleValidation()){
+        
+        const {password, username, email} = values;
+        const {data} = await axios.post(registerRoute,{username,
+        email,
+    password,
+    });
+    if(data.status===false){
+        toast.error(data.msg, toastOptions);
+    }
+    if(data.status === true){
+        localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+        navigate("/");
+    }
+        
+       }
     };
 
     const handleValidation =() => {
         const {password,confirmPassword,username,email} = values;
         if(password!==confirmPassword){
-            alert("as");
-            toast.error("password and confirm password should be same.",{
-                position: "bottom-right",
-                autoClose: 8000,
-                pauseOnHover: true,
-                draggable: true,
-                theme: "dark",
+            
+            toast.error("password and confirm password should be same.", toastOptions
+               );
+               return false;
+        } else if (username.length<3){
+            toast.error("Username should be greater than 3 characters", toastOptions 
+            );
+            return false;
 
-            });
+        } else if (password.length<8){
+            toast.error("Password should be equal and greater than 8 characters", toastOptions 
+            );
+            return false;
+        } else if (email===""){
+            toast.error("email is required", toastOptions);
+            return false;
         }
+        return true;
+            
     };
 
     const handleChange = (event) => {
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const userRoutes= require("./routes/userRouts");
 
 
 const app = express();
@@ -9,7 +9,10 @@ require("dotenv").config();
 
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+app.use("/api/auth", userRoutes)
+
 
 
 mongoose.connect(process.env.MONGO_URL,{
